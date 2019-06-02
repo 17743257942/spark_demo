9dd7088acd845a115103b681a951b40e250e92ea
scx2019 <546294903@qq.com>
2019-06-02 13:11:55 +0800

    asdf

--- src/main/scala/spark_demo/SparkDemo.scala
+++ src/main/scala/spark_demo/SparkDemo.scala
@@ -27,14 +27,22 @@ object SparkDemo {
     /**
       * 第二种wc
       */
-    val lines = rdd.flatMap(line => line.split(" "))
+    val lines = rdd.flatMap(line => line.split(" ")) // 压扁文档，将所有行汇聚
     val count1 = lines.map(word => (word, 1)) // 使用map算子将元素变成键值对
     println(count1.collect().mkString(","))
-    val count2 = count1.reduceByKey { case (x, y) => x + y } //case可以去掉，第一个元素加第二个
-//    val count2 = count1.reduceByKey { _ + _ }
+    val count2 = count1.reduceByKey { case (x, y) => x + y } //case可以去掉
+//    val count2 = count1.reduceByKey { _ + _ } //不同RDD中相同key值拉到一起进行value的归并操作
     println(count2.collect().mkString(","))
+    val count3 = count2.map(x => (x._2, x._1)) // k v 调转顺序 变成<v,k>
+    println(count3.collect().mkString(","))
+    val count4 = count3.sortByKey(false) // 按 v 降序排列
+    println(count4.collect().mkString(","))
+    val count5 = count4.map(x => (x._2, x._1)) // 再调转顺序，恢复原样<k,v>
+    println(count5.collect().mkString(","))
     //    count.saveAsTextFile("E:\\Program Files\\vm_linux\\share\\aaout2.txt") //保存到一个不存在的目录
-
+    println()
+    println()
+    println()
 
     val rdd2 = rdd.flatMap(_.split(" "))
     println(rdd2.collect().mkString(","))  //hello,world,hello,my,world,hello,world,of,mine,hello,worlds
