891a24207370a0de1f7db58db77b19a429c3d3f6
scx2019 <546294903@qq.com>
2019-06-02 10:08:29 +0800

    asdf

--- src/main/scala/spark_demo/SparkDemo.scala
+++ src/main/scala/spark_demo/SparkDemo.scala
@@ -6,13 +6,16 @@ object SparkDemo {
   def main(args: Array[String]): Unit = {
     val logFile = "E:\\Program Files\\vm_linux\\share\\aa.txt"
     val conf = new SparkConf()
-    conf.setMaster("local[4]")
+    conf.setMaster("local[1]")
     conf.setAppName("test0")
     conf.set("spark.testing.memory", "5000000000")
     val sc = new SparkContext(conf)
     println("create my first SparkContext ----------")
 
     val rdd = sc.textFile(logFile)
+    /**
+      * 第一种wc
+      */
     //    val wordcount = rdd.flatMap(_.split(" "))
     //      .map((_, 1)).reduceByKey(_ + _)
     //      .map(x => (x._2, x._1))
@@ -20,6 +23,13 @@ object SparkDemo {
     //      .map(x => (x._2, x._1))
     //    wordcount.saveAsTextFile("E:\\Program Files\\vm_linux\\share\\aaout.txt")
     //    sc.stop()
+    /**
+      * 第二种wc
+      */
+    val lines = rdd.flatMap(line=>line.split(" "))
+    val count = lines.map(word => (word,1)).reduceByKey{case (x,y)=>x+y}
+    count.saveAsTextFile("E:\\Program Files\\vm_linux\\share\\aaout2.txt")
+
 
     val rdd2 = rdd.flatMap(_.split(" "))
     println(rdd2.collect().mkString(","))
