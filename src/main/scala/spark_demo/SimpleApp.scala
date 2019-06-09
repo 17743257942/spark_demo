e89cecdac1504fbf721d7607cc76886d90550ff9
scx2019 <546294903@qq.com>
2019-06-10 05:44:11 +0800

    asdf

--- src/main/scala/spark_demo/SimpleApp.scala
+++ src/main/scala/spark_demo/SimpleApp.scala
@@ -5,13 +5,13 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
 
   def main(args: Array[String]) {
-    val logFile = this.getClass().getResource("aa.txt").toString.substring(6) // Should be some file on your system
+    val logFile = this.getClass().getResource("dota.txt").toString.substring(6) // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").master("local[1]")
       .config("spark.testing.memory", "5000000000").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val numAs = logData.filter(line => line.contains("亡灵")).count()
+    val numBs = logData.filter(line => line.contains("火女")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs") //Lines with a: 4, Lines with b: 2
     spark.stop()
 
 
