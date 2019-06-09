b1646445f716a324e3bf589dfb996d9e63da5043
scx2019 <546294903@qq.com>
2019-06-09 18:11:12 +0800

    asdf

--- src/main/scala/spark_demo/SparkDemo.scala
+++ src/main/scala/spark_demo/SparkDemo.scala
@@ -5,7 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkDemo {
   def main(args: Array[String]): Unit = {
-//    val logFile = "E:\\Program Files\\vm_linux\\share\\aa.txt"
+    //    val logFile = "E:\\Program Files\\vm_linux\\share\\aa.txt"
     val logFile = this.getClass().getResource("aa.txt").toString.substring(6)
     val conf = new SparkConf()
     conf.setMaster("local[1]")
@@ -29,12 +29,13 @@ object SparkDemo {
     /**
       * 第二种wc
       */
+    println(rdd.collect().mkString(",")) //hello world,hello my world,hello world of mine,hello worlds
     val lines = rdd.flatMap(line => line.split(" ")) // 压扁文档，将所有行汇聚
-    println(rdd.collect().mkString(","))
+    println(lines.collect().mkString(",")) //hello,world,hello,my,world,hello,world,of,mine,hello,worlds
     val count1 = lines.map(word => (word, 1)) // 使用map算子将元素变成键值对
     println(count1.collect().mkString(","))
     val count2 = count1.reduceByKey { case (x, y) => x + y } //case可以去掉
-//    val count2 = count1.reduceByKey { _ + _ } //不同RDD中相同key值拉到一起进行value的归并操作
+    //    val count2 = count1.reduceByKey { _ + _ } //不同RDD中相同key值拉到一起进行value的归并操作
     println(count2.collect().mkString(","))
     val count3 = count2.map(x => (x._2, x._1)) // k v 调转顺序 变成<v,k>
     println(count3.collect().mkString(","))
@@ -49,7 +50,7 @@ object SparkDemo {
 
 
     val rdd22 = rdd.flatMap(_.split(" "))
-    println(rdd22.collect().mkString(","))  //hello,world,hello,my,world,hello,world,of,mine,hello,worlds
+    println(rdd22.collect().mkString(",")) //hello,world,hello,my,world,hello,world,of,mine,hello,worlds
     val rdd3 = rdd.map(_.split(" "))
     println(rdd3.first()) // [Ljava.lang.String;@3f3c96
     println(rdd3.count()) // 4
@@ -74,16 +75,14 @@ object SparkDemo {
     println(r04.collect().mkString(","))
 
 
+    //    val rddInt:RDD[Int] = sc.makeRDD(List(1,2,3,4,5,6,2,5,1))
+    val rddInt: RDD[Int] = sc.makeRDD(Array(1, 2, 3, 4, 5, 6, 2, 5, 1))
+    //    val rddStr:RDD[String] = sc.parallelize(Array("a","b","c","d","b","a"), 1)
+    val rddStr: RDD[String] = sc.parallelize(List("a", "b", "c", "d", "b", "a"), 1)
+    val rddFile2: RDD[String] = sc.textFile(logFile, 1)
 
-
-//    val rddInt:RDD[Int] = sc.makeRDD(List(1,2,3,4,5,6,2,5,1))
-    val rddInt:RDD[Int] = sc.makeRDD(Array(1,2,3,4,5,6,2,5,1))
-//    val rddStr:RDD[String] = sc.parallelize(Array("a","b","c","d","b","a"), 1)
-    val rddStr:RDD[String] = sc.parallelize(List("a","b","c","d","b","a"), 1)
-    val rddFile2:RDD[String] = sc.textFile(logFile, 1)
-
-    val rdd01:RDD[Int] = sc.makeRDD(List(1,3,5,3))
-    val rdd02:RDD[Int] = sc.makeRDD(List(2,4,5,1))
+    val rdd01: RDD[Int] = sc.makeRDD(List(1, 3, 5, 3))
+    val rdd02: RDD[Int] = sc.makeRDD(List(2, 4, 5, 1))
     /* map操作 */
     println("======map操作======")
     println(rddInt.map(x => x + 1).collect().mkString(","))
@@ -113,7 +112,7 @@ object SparkDemo {
     println()
     println()
 
-      // Pair RDD及键值对RDD，Spark里创建Pair RDD也是可以通过两种途径，一种是从内存里读取，一种是从文件读取。
+    // Pair RDD及键值对RDD，Spark里创建Pair RDD也是可以通过两种途径，一种是从内存里读取，一种是从文件读取。
     /**
       * reduceByKey：合并具有相同键的值；
       * groupByKey：对具有相同键的值进行分组；
@@ -137,91 +136,107 @@ object SparkDemo {
       * sample：对RDD里的数据采样；
       * takeOrdered：从RDD里按照提供的顺序返回最前面的num个元素
       */
-    val sparkdata01=  this.getClass().getResource("sparkdata01.txt").toString.substring(6)
-    val rddFile:RDD[(String,String)] = sc.textFile(sparkdata01, 1)
-      .map { x => (x.split(",")(0),x.split(",")(1) + "," + x.split(",")(2)) }
-    println(rddFile.collect().mkString(","))
-    val rFile:RDD[String] = rddFile.keys
-    val rFile2:RDD[String] = rddFile.values
+    val sparkdata01 = this.getClass().getResource("sparkdata01.txt").toString.substring(6)
+    val rddFile: RDD[(String, String)] = sc.textFile(sparkdata01, 1)
+      .map { x => (x.split(",")(0), x.split(",")(1) + "," + x.split(",")(2)) }
+    println(rddFile.collect().mkString(",")) // (x01,1,4),(x02,11,1),(x01,3,9),(x01,2,6),(x02,18,12),(x03,7,9)
+    val rFile: RDD[String] = rddFile.keys
+    val rFile2: RDD[String] = rddFile.values
     println("=========createPairMap File=========")
-    println(rFile.collect().mkString(","))// x01,x02,x01,x01,x02,x03
-    println(rFile2.collect().mkString("  "))//1,4  11,1  3,9  2,6  18,12  7,9
+    println(rFile.collect().mkString(",")) // x01,x02,x01,x01,x02,x03
+    println(rFile2.collect().mkString(",")) //1,4,11,1,3,9,2,6,18,12,7,9
 
-    val rdd2:RDD[(String,Int)] = sc.makeRDD(List(("k01",3),("k02",6),("k03",2),("k01",26)))
-    val r:RDD[(String,Int)] = rdd2.reduceByKey((x,y) => x + y)
+    val rdd2: RDD[(String, Int)] = sc.makeRDD(List(("k01", 3), ("k02", 6), ("k03", 2), ("k01", 26)))
+    val r: RDD[(String, Int)] = rdd2.reduceByKey((x, y) => x + y)
     println("=========createPairMap=========")
-    println(r.collect().mkString(","))// (k01,29),(k03,2),(k02,6)
+    println(r.collect().mkString(",")) // (k01,29),(k03,2),(k02,6)
 
-    val other:RDD[(String,Int)] = sc.parallelize(List(("k01",29)), 1)
+    val other: RDD[(String, Int)] = sc.parallelize(List(("k01", 29)), 1)
 
     // 转化操作
-    val rddReduce:RDD[(String,Int)] = rdd2.reduceByKey((x,y) => x + y)
-    println("====reduceByKey===:" + rddReduce.collect().mkString(","))// (k01,29),(k03,2),(k02,6)
-    val rddGroup:RDD[(String,Iterable[Int])] = rdd2.groupByKey()
-    println("====groupByKey===:" + rddGroup.collect().mkString(","))// (k01,CompactBuffer(3, 26)),(k03,CompactBuffer(2)),(k02,CompactBuffer(6))
-    val rddKeys:RDD[String] = rdd2.keys
-    println("====keys=====:" + rddKeys.collect().mkString(","))// k01,k02,k03,k01
-    val rddVals:RDD[Int] = rdd2.values
-    println("======values===:" + rddVals.collect().mkString(","))// 3,6,2,26
-    val rddSortAsc:RDD[(String,Int)] = rdd2.sortByKey(true, 1)
-    val rddSortDes:RDD[(String,Int)] = rdd2.sortByKey(false, 1)
-    println("====rddSortAsc=====:" + rddSortAsc.collect().mkString(","))// (k01,3),(k01,26),(k02,6),(k03,2)
-    println("======rddSortDes=====:" + rddSortDes.collect().mkString(","))// (k03,2),(k02,6),(k01,3),(k01,26)
-    val rddFmVal:RDD[(String,Int)] = rdd2.flatMapValues { x => List(x + 10) }
-    println("====flatMapValues===:" + rddFmVal.collect().mkString(","))// (k01,13),(k02,16),(k03,12),(k01,36)
-    val rddMapVal:RDD[(String,Int)] = rdd2.mapValues { x => x + 10 }
-    println("====mapValues====:" + rddMapVal.collect().mkString(","))// (k01,13),(k02,16),(k03,12),(k01,36)
-    val rddCombine:RDD[(String,(Int,Int))] = rdd2.combineByKey(x => (x,1), (param:(Int,Int),x) => (param._1 + x,param._2 + 1), (p1:(Int,Int),p2:(Int,Int)) => (p1._1 + p2._1,p1._2 + p2._2))
-    println("====combineByKey====:" + rddCombine.collect().mkString(","))//(k01,(29,2)),(k03,(2,1)),(k02,(6,1))
-    val rddSubtract:RDD[(String,Int)] = rdd2.subtractByKey(other);
-    println("====subtractByKey====:" + rddSubtract.collect().mkString(","))// (k03,2),(k02,6)
-    val rddJoin:RDD[(String,(Int,Int))] = rdd2.join(other)
-    println("=====rddJoin====:" + rddJoin.collect().mkString(","))// (k01,(3,29)),(k01,(26,29))
-    val rddRight:RDD[(String,(Option[Int],Int))] = rdd2.rightOuterJoin(other)
-    println("====rightOuterJoin=====:" + rddRight.collect().mkString(","))// (k01,(Some(3),29)),(k01,(Some(26),29))
-    val rddLeft:RDD[(String,(Int,Option[Int]))] = rdd2.leftOuterJoin(other)
-    println("=====rddLeft=====:" + rddLeft.collect().mkString(","))// (k01,(3,Some(29))),(k01,(26,Some(29))),(k03,(2,None)),(k02,(6,None))
+    val rddReduce: RDD[(String, Int)] = rdd2.reduceByKey((x, y) => x + y)
+    println("====reduceByKey===:" + rddReduce.collect().mkString(","))
+    // (k01,29),(k03,2),(k02,6)
+    val rddGroup: RDD[(String, Iterable[Int])] = rdd2.groupByKey()
+    println("====groupByKey===:" + rddGroup.collect().mkString(","))
+    // (k01,CompactBuffer(3, 26)),(k03,CompactBuffer(2)),(k02,CompactBuffer(6))
+    val rddKeys: RDD[String] = rdd2.keys
+    println("====keys=====:" + rddKeys.collect().mkString(","))
+    // k01,k02,k03,k01
+    val rddVals: RDD[Int] = rdd2.values
+    println("======values===:" + rddVals.collect().mkString(","))
+    // 3,6,2,26
+    val rddSortAsc: RDD[(String, Int)] = rdd2.sortByKey(true, 1)
+    val rddSortDes: RDD[(String, Int)] = rdd2.sortByKey(false, 1)
+    println("====rddSortAsc=====:" + rddSortAsc.collect().mkString(",")) // (k01,3),(k01,26),(k02,6),(k03,2)
+    println("======rddSortDes=====:" + rddSortDes.collect().mkString(",")) // (k03,2),(k02,6),(k01,3),(k01,26)
+
+    val rdd222: RDD[(String, List[Int])] = sc.parallelize(List(("a", List(3, 2, 1)), ("b", List(5, 6))))
+    println(rdd222.collect().mkString(",")) //(a,List(3, 2, 1)),(b,List(5, 6))
+    val rddFmVal2: RDD[(String, Int)] = rdd222.flatMapValues { x => x.iterator }
+    println("====flatMapValues===:" + rddFmVal2.collect().mkString(",")) // (a,3),(a,2),(a,1),(b,5),(b,6)
+    val rddFmVal3: RDD[(String, Int)] = rddFmVal2.mapValues { x => x + 10 }
+    println("====flatMapValues===:" + rddFmVal3.collect().mkString(",")) // (a,13),(a,12),(a,11),(b,15),(b,16)
+
+    val rddFmVal: RDD[(String, Int)] = rdd2.flatMapValues { x => List(x + 10) }
+    println("====flatMapValues===:" + rddFmVal.collect().mkString(","))
+    // (k01,13),(k02,16),(k03,12),(k01,36)
+    val rddMapVal: RDD[(String, Int)] = rdd2.mapValues { x => x + 10 }
+    println("====mapValues====:" + rddMapVal.collect().mkString(","))
+    // (k01,13),(k02,16),(k03,12),(k01,36)
+
+    // 难点
+    val rddCombine: RDD[(String, (Int, Int))] = rdd2.combineByKey(
+      x => (x, 1), //初始化
+      (param: (Int, Int), x) => (param._1 + x, param._2 + 1), // param元组类型，上一步的结果；x是新rdd2中的值
+      (p1: (Int, Int), p2: (Int, Int)) => (p1._1 + p2._1, p1._2 + p2._2) //对结果进行汇总
+    )
+    println("====combineByKey====:" + rddCombine.collect().mkString(","))
+    //(k01,(29,2)),(k03,(2,1)),(k02,(6,1))
+
+    val rddSubtract: RDD[(String, Int)] = rdd2.subtractByKey(other);
+    println("====subtractByKey====:" + rddSubtract.collect().mkString(","))
+    // (k03,2),(k02,6)
+    val rddJoin: RDD[(String, (Int, Int))] = rdd2.join(other)
+    println("=====rddJoin====:" + rddJoin.collect().mkString(","))
+    // (k01,(3,29)),(k01,(26,29))
+    val rddRight: RDD[(String, (Option[Int], Int))] = rdd2.rightOuterJoin(other)
+    println("====rightOuterJoin=====:" + rddRight.collect().mkString(","))
+    // (k01,(Some(3),29)),(k01,(Some(26),29))
+    val rddLeft: RDD[(String, (Int, Option[Int]))] = rdd2.leftOuterJoin(other)
+    println("=====rddLeft=====:" + rddLeft.collect().mkString(","))
+    // (k01,(3,Some(29))),(k01,(26,Some(29))),(k03,(2,None)),(k02,(6,None))
     val rddCogroup: RDD[(String, (Iterable[Int], Iterable[Int]))] = rdd2.cogroup(other)
-    println("=====cogroup=====:" + rddCogroup.collect().mkString(","))// (k01,(CompactBuffer(3, 26),CompactBuffer(29))),(k03,(CompactBuffer(2),CompactBuffer())),(k02,(CompactBuffer(6),CompactBuffer()))
+    println("=====cogroup=====:" + rddCogroup.collect().mkString(","))
+    // (k01,(CompactBuffer(3, 26),CompactBuffer(29))),(k03,(CompactBuffer(2),CompactBuffer())),(k02,(CompactBuffer(6),CompactBuffer()))
     // 行动操作
     val resCountByKey = rdd2.countByKey()
-    println("=====countByKey=====:" + resCountByKey)// Map(k01 -> 2, k03 -> 1, k02 -> 1)
+    println("=====countByKey=====:" + resCountByKey)
+    // Map(k01 -> 2, k03 -> 1, k02 -> 1)
     val resColMap = rdd2.collectAsMap()
-    println("=====resColMap=====:" + resColMap)//Map(k02 -> 6, k01 -> 26, k03 -> 2)
+    println("=====resColMap=====:" + resColMap)
+    //Map(k02 -> 6, k01 -> 26, k03 -> 2)
     val resLookup = rdd2.lookup("k01")
     println("====lookup===:" + resLookup) // WrappedArray(3, 26)
 
     // 其他一些不常用的RDD操作
-    println("=====first=====:" + rdd.first())//(k01,3)
+    println("=====first=====:" + rdd.first())
+    //(k01,3)
     val resTop = rdd2.top(2).map(x => x._1 + ";" + x._2)
-    println("=====top=====:" + resTop.mkString(","))// k03;2,k02;6
+    println("=====top=====:" + resTop.mkString(","))
+    // k03;2,k02;6
     val resTake = rdd2.take(2).map(x => x._1 + ";" + x._2)
-    println("=======take====:" + resTake.mkString(","))// k01;3,k02;6
+    println("=======take====:" + resTake.mkString(","))
+    // k01;3,k02;6
     val resTakeSample = rdd2.takeSample(false, 2).map(x => x._1 + ";" + x._2)
-    println("=====takeSample====:" + resTakeSample.mkString(","))// k01;26,k03;2
+    println("=====takeSample====:" + resTakeSample.mkString(","))
+    // k01;26,k03;2
     val resSample1 = rdd2.sample(false, 0.25)
     val resSample2 = rdd2.sample(false, 0.75)
     val resSample3 = rdd2.sample(false, 0.5)
-    println("=====sample======:" + resSample1.collect().mkString(","))// 无
-    println("=====sample======:" + resSample2.collect().mkString(","))// (k01,3),(k02,6),(k01,26)
-    println("=====sample======:" + resSample3.collect().mkString(","))// (k01,3),(k01,26)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    println("=====sample======:" + resSample1.collect().mkString(",")) // 无
+    println("=====sample======:" + resSample2.collect().mkString(",")) // (k01,3),(k02,6),(k01,26)
+    println("=====sample======:" + resSample3.collect().mkString(",")) // (k01,3),(k01,26)
 
 
   }
---
+++ src/main/scala/spark_demo/SparkSession.scala
@@ -0,0 +1,8 @@
+package spark_demo
+
+/**
+  * 2019年6月9日
+  */
+object SparkSession {
+
+}
