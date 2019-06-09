03696c1bd18fffd08a523977889fb2c2d16be9b6
scx2019 <546294903@qq.com>
2019-06-10 05:41:18 +0800

    asdf

---
+++ src/main/scala/spark_demo/SimpleApp.scala
@@ -0,0 +1,19 @@
+package spark_demo
+
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+
+  def main(args: Array[String]) {
+    val logFile = this.getClass().getResource("aa.txt").toString.substring(6) // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").master("local[1]")
+      .config("spark.testing.memory", "5000000000").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+
+
+  }
+}
--- src/main/scala/spark_demo/SparkSession.scala
+++
@@ -1,8 +0,0 @@
-package spark_demo
-
-/**
-  * 2019年6月9日
-  */
-object SparkSession {
-
-}
