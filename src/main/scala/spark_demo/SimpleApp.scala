cc63f7abe8f00d55ef9bd2a3d3a61ecfd191af1f
scx2019 <546294903@qq.com>
2019-06-12 23:30:47 +0800

    asdf

--- src/main/scala/spark_demo/SimpleApp.scala
+++ src/main/scala/spark_demo/SimpleApp.scala
@@ -5,8 +5,12 @@ import org.apache.spark.sql.SparkSession
 object SimpleApp {
 
   def main(args: Array[String]) {
-    //    sessionDemo1
+    sessionDemo1
+    println("===============================================================")
     sessionDemo2
+    println("===============================================================")
+    sessionDemo3
+    println("===============================================================")
 
 
   }
@@ -31,6 +35,8 @@ object SimpleApp {
       //      .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
+    //    val df0 = spark.read.format("json").load(logFile)
+    //    df0.show()
 
     /**
       * +----+---+-----------+
@@ -45,21 +51,32 @@ object SimpleApp {
       ("ming", 20, 15552211521L),
       ("hong", 19, 13287994007L),
       ("zhi", 21, 15552211523L)
-      ))
-//    toDF("name", "age", "phone")
-    val df1 = df.withColumnRenamed("_1","name")
-      .withColumnRenamed("_2","age")
-      .withColumnRenamed("_3","phone")
-      .withColumnRenamed("_4","qwer") // 没有这列，所以不显示
+    ))
+    //    toDF("name", "age", "phone")
+    val df1 = df.withColumnRenamed("_1", "name")
+      .withColumnRenamed("_2", "age")
+      .withColumnRenamed("_3", "phone")
+      .withColumnRenamed("_4", "qwer") // 没有这列，所以不显示
     df1.show()
-    val df2 = df1.withColumnRenamed("name","asdf")
-    //    val df = spark.read.format("json").load(logFile)
+    val df2 = df1.withColumnRenamed("name", "asdf")
     df2.show()
 
+    df1.createOrReplaceTempView("people") //对上面的dataframe创建一个表
+    df1.cache()
+    //缓存表
+    val resultsDF = spark.sql("SELECT name, age, phone FROM people where age<20") //对表调用SQL语句
+    resultsDF.show(10) //展示结果
 
+  }
 
-
-
-
+  private def sessionDemo3 = {
+    val logFile = this.getClass().getResource("dota.txt").toString.substring(6) // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").master("local[1]")
+      .config("spark.testing.memory", "5000000000").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("亡灵")).count()
+    val numBs = logData.filter(line => line.contains("火女")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs") //Lines with a: 4, Lines with b: 2
+    spark.stop()
   }
 }
